727956522a394ffe1362a30b1c77c5ebecf3291a
petersaalbrink <petersaalbrink@gmail.com>
2019-08-07 10:47:34 +0200

    Removed ignores

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from common.classes import ESClient, EmailClient, MySQLClient, MongoDB
---
+++ secrets.py
@@ -0,0 +1,4 @@
+mongo = ("devpsaalbrink", b"Y0c3dWVWTks=")
+sql = ("trainee_peter", b"cGV0ZXIwMQ==")
+es = ("psaalbrink@matrixiangroup.com", b"ODJNJTBdbW5lJ252")
+mail_pass = b"TmtUZ01wbThvVDNjSzk1NA=="
