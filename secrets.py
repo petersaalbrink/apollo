35b6078bfd07b658318315c32281053e4a6532ff
petersaalbrink <petersaalbrink@gmail.com>
2019-08-12 15:41:43 +0200

    Added ftp access

--- secrets.py
+++ secrets.py
@@ -2,3 +2,4 @@ mongo = ("devpsaalbrink", b"Y0c3dWVWTks=")
 sql = ("trainee_peter", b"cGV0ZXIwMQ==")
 es = ("psaalbrink@matrixiangroup.com", b"ODJNJTBdbW5lJ252")
 mail_pass = b"TmtUZ01wbThvVDNjSzk1NA=="
+ccv = ("CCV", b"WGpPNko4NEU=")
