1381cca83e3dcb1c74f7a77fadf21fe71f1942c1
psaalbrink <psaalbrink@matrixiangroup.com>
2019-08-31 10:36:11 +0200

    Trying to get address_checker to work

--- setup.py
+++ setup.py
@@ -9,14 +9,35 @@ setuptools.setup(
     version="0.0.1",
     author="Peter Saalbrink",
     author_email="psaalbrink@matrixiangroup.com",
+    keywords="common",
     description="Common classes",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://bitbucket.org/matrixiangroup_dev/common_classes_peter",
     packages=setuptools.find_packages(),
+    package_data={"": [
+        "client-cert.pem",
+        "client-key.pem",
+        "server-ca.pem",
+        "achternamen_spreiding_clean.csv",
+        "titelatuur.csv",
+        "voornamen_spreiding_clean.csv"
+    ]},
+    data_files=[
+        ("certificates", [
+            "certificates/client-cert.pem",
+            "certificates/client-key.pem",
+            "certificates/server-ca.pem"
+        ]),
+        ("data", [
+            "data/achternamen_spreiding_clean.csv",
+            "data/titelatuur.csv",
+            "data/voornamen_spreiding_clean.csv"
+        ])],
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
+    ]
 )
