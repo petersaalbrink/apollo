eb18d4679cd08a5134af3025b747abb266f0b033
psaalbrink <psaalbrink@matrixiangroup.com>
2019-09-12 16:20:19 +0200

    Add RadarPlot to init

--- common/__init__.py
+++ common/__init__.py
@@ -1,6 +1,7 @@
 from common.connectors import Timer, ESClient, EmailClient, MySQLClient, MongoDB
 from common.persondata import NoMatch, Match, NamesData
 from common.handlers import ZipData, csv_write
+from common.visualizations import RadarPlot
 from common.parsers import parse
 from common.parsers import parse as parser
 from common.parsers import parse as parser_final
