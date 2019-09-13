5daabbf2135699b4b8e41834a15f193b2556d7c0
psaalbrink <psaalbrink@matrixiangroup.com>
2019-09-13 11:20:02 +0200

    Added Checks class

--- common/__init__.py
+++ common/__init__.py
@@ -2,6 +2,6 @@ from common.connectors import Timer, ESClient, EmailClient, MySQLClient, MongoDB
 from common.persondata import NoMatch, Match, NamesData
 from common.handlers import ZipData, csv_write
 from common.visualizations import RadarPlot
-from common.parsers import parse
+from common.parsers import parse, Checks
 from common.parsers import parse as parser
 from common.parsers import parse as parser_final
--- common/parsers.py
+++ common/parsers.py
@@ -17,3 +17,55 @@ def parse(address: str, country: str = "NL"):
         "NL": dutch_parser(address),
         "UK": english_parser(address),
     }.get(country, "global")
+
+
+class Checks:
+    """Collection of several methods for preparation of MySQL data for MongoDB insertion."""
+
+    @staticmethod
+    def percentage(part, whole):
+        return round(100 * float(part) / float(whole), 2)
+
+    @staticmethod
+    def int_or_null(var):
+        try:
+            return int(var) if var is not None else None
+        except ValueError:
+            return None
+
+    @staticmethod
+    def bool_or_null(var):
+        return bool(Checks.int_or_null(var)) if var is not None else None
+
+    @staticmethod
+    def str_or_null(var):
+        return str(var) if var else None
+
+    @staticmethod
+    def str_or_empty(var):
+        return str(var) if var else ""
+
+    @staticmethod
+    def float_or_null(var):
+        return float(var) if var is not None else None
+
+    @staticmethod
+    def check_null(var):
+        return var if var else None
+
+    @staticmethod
+    def energy_label(var):
+        return {
+            "A": 7,
+            "A+": 7,
+            "A++": 7,
+            "A+++": 7,
+            "A++++": 7,
+            "B": 6,
+            "C": 5,
+            "D": 4,
+            "E": 3,
+            "F": 2,
+            "G": 1,
+            None: 0
+        }.get(var, 0)
