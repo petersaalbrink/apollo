3f100b5f04d8ddfdbfa1df08ed7caa4b2a628e5f
psaalbrink <psaalbrink@matrixiangroup.com>
2019-09-30 10:12:14 +0200

    Add visualizations.DistributionPlot to common.__init__

--- common/__init__.py
+++ common/__init__.py
@@ -1,7 +1,7 @@
 from common.connectors import ESClient, EmailClient, MySQLClient, MongoDB
 from common.persondata import NoMatch, Match, NamesData
 from common.handlers import Timer, ZipData, csv_write, csv_read
-from common.visualizations import RadarPlot
+from common.visualizations import RadarPlot, DistributionPlot
 from common.parsers import parse, Checks
 from common.parsers import parse as parser
 from common.parsers import parse as parser_final
