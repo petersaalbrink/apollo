f511070375e08523680e3ba90661ac3a8f79b903
psaalbrink <psaalbrink@matrixiangroup.com>
2019-10-10 16:57:17 +0200

    Remove weird docstring

--- common/handlers.py
+++ common/handlers.py
@@ -43,14 +43,6 @@ class Log:
         import sys
         import logging
         self.level = {
-            """CRITICAL = 50
-                FATAL = CRITICAL
-                ERROR = 40
-                WARNING = 30
-                WARN = WARNING
-                INFO = 20
-                DEBUG = 10
-                NOTSET = 0"""
             "notset": logging.NOTSET,
             "debug": logging.DEBUG,
             "info": logging.INFO,
