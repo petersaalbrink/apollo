6c7c8094b83f2eb66026dd2259814772671f7444
psaalbrink <psaalbrink@matrixiangroup.com>
2019-10-31 17:01:02 +0100

    Add `plot_stacked_bar` function

--- common/__init__.py
+++ common/__init__.py
@@ -1,7 +1,7 @@
 from common.connectors import ESClient, EmailClient, MySQLClient, MongoDB
 from common.persondata import NoMatch, PersonMatch, NamesData, PhoneNumberFinder
 from common.handlers import Timer, ZipData, csv_write, csv_read, Log, get, thread, send_email
-from common.visualizations import RadarPlot, DistributionPlot
+from common.visualizations import RadarPlot, DistributionPlot, plot_stacked_bar
 from common.parsers import parse, Checks
 
 __version__ = "0.0.2"
