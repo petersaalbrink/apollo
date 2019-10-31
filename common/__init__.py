ce2a15b38ca16db02836126ecb4ba306dcab1a2b
psaalbrink <psaalbrink@matrixiangroup.com>
2019-10-31 21:36:47 +0100

    Use versioning properly, so we won't have import errors when trying to run setup

--- common/__init__.py
+++ common/__init__.py
@@ -1,7 +1,6 @@
-from common.connectors import ESClient, EmailClient, MySQLClient, MongoDB
-from common.persondata import NoMatch, PersonMatch, NamesData, PhoneNumberFinder
-from common.handlers import Timer, ZipData, csv_write, csv_read, Log, get, thread, send_email
-from common.visualizations import RadarPlot, DistributionPlot, plot_stacked_bar
-from common.parsers import parse, Checks
-
-__version__ = "0.0.2"
+from .connectors import ESClient, EmailClient, MySQLClient, MongoDB
+from .persondata import NoMatch, PersonMatch, NamesData, PhoneNumberFinder
+from .handlers import Timer, ZipData, csv_write, csv_read, Log, get, thread, send_email
+from .visualizations import RadarPlot, DistributionPlot, plot_stacked_bar
+from .parsers import parse, Checks
+from ._version import __version__
