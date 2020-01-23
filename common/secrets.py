3e38d09866f2b12411eec553813f663432260c36
psaalbrink <psaalbrink@matrixiangroup.com>
2020-01-23 16:01:47 +0100

    Use prod API for Address Checker

--- common/secrets.py
+++ common/secrets.py
@@ -1,6 +1,9 @@
 from base64 import b64encode, b64decode
 from collections import namedtuple
+from contextlib import suppress
+from json import loads
 from pathlib import Path
+from requests import post
 try:
     from getpass import getpass
     import termios
@@ -73,3 +76,19 @@ def get_secret(name: str) -> Credentials:
             f.write(f"{name}::{usr}::{pwd}\n")
 
     return Credentials(usr, b64decode(bytes(pwd.encode())).decode())
+
+
+def get_token() -> dict:
+    usr, pwd = get_secret("data_platform")
+    while True:
+        with suppress(KeyError):
+            posted = post("https://api.matrixiangroup.com/token",
+                          data={"username": usr,
+                                "password": pwd})
+            token = loads(posted.text)["access_token"]
+            break
+    return {
+        "Accept": "application/json",
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
