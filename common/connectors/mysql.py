f2f418f8df54cd0b9bca6a35c4da2b91e4ac0bd7
psaalbrink <psaalbrink@matrixiangroup.com>
2020-02-27 13:31:10 +0100

    Set `database` and `table_name` attributes in `MySQLClient.build`

--- common/connectors/mysql.py
+++ common/connectors/mysql.py
@@ -914,6 +914,11 @@ class MySQLClient:
             table = f"{self.database}.{self.table_name}"
         elif "." not in table:
             table = f"{self.database}.{table}"
+        elif not self.table_name:
+            if "." in table:
+                self.database, self.table_name = table.split(".")
+            else:
+                self.table_name = table
 
         if fields_as:
             if isinstance(select_fields, str):
