4c45d6d477e1466ffd9015075ea8fd46f9fa1634
psaalbrink <psaalbrink@matrixiangroup.com>
2021-01-18 22:46:46 +0100

    minor setup update

--- setup.py
+++ setup.py
@@ -34,5 +34,5 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7,<3.10",
+    python_requires=">=3.7,<3.11",
 )
